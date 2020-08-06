e7ff48ddbbcc16bbeae945ab944098a62d3dd07e
Ira Vakhnina <pirania943@gmail.com>
2020-08-06 15:01:54 +0300

    fixed

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,23 +1,25 @@
 "use strict";
 
 const userForm = new UserForm();
+userForm.loginFormCallback = function(data){
+    ApiConnector.login(data, (response) => {
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.data);
+        }
+    })
+} 
 
-userForm.loginFormCallback = (data = {login: null, password: null}) => {
-	ApiConnector.login(data, (response) => {
-		if (response.success) {
-			location.reload();
-		} else {
-			alert(response.data);
-		}
-	})
-}
 
-userForm.registerFormCallback = (data = {login: null, password: null}) => {
-	ApiConnector.register(data, (response) => {
-		if (response.success) {
-			location.reload();
-		} else {
-			alert(response.data);
-		}
-	})
+userForm.registerFormCallback = function(data){
+    ApiConnector.register(data, (response) =>{
+        console.log(response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.data);
+        }
+    })
 }
